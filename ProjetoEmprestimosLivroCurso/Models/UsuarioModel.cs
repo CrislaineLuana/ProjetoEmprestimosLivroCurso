00fe18647b4adc9d194569a84057cf0e32c3e7ce
Crislaine <clpaula3007@gmail.com>
2024-01-18 10:32:48 -0300

    Estrutura MVC

---
+++ ProjetoEmprestimosLivroCurso/Models/UsuarioModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjetoEmprestimosLivroCurso.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataDeCadastro { get; set; }
+    }
+}
