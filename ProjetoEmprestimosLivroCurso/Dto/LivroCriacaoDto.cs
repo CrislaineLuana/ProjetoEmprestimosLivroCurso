3de7cab8514182ec0c86df290732f2dd38ad2133
Crislaine <clpaula3007@gmail.com>
2024-01-24 11:25:25 -0300

    ExplicandoDto

---
+++ ProjetoEmprestimosLivroCurso/Dto/LivroCriacaoDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoEmprestimosLivroCurso.Dto
+{
+    public class LivroCriacaoDto
+    {
+        [Required(ErrorMessage = "Insira um título!")]
+        public string Titulo { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Insira um descrição!")]
+        public string Descricao { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Insira um capa!")]
+        public string Capa { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Insira o código ISBN!")]
+        public string ISBN { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Insira o autor!")]
+        public string Autor { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Insira o Genero!")]
+        public string Genero { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Insira o ano de publicacao!")]
+        public int AnoPublicacao { get; set; }
+        [Required(ErrorMessage = "Insira uma quantidade em estoque!")]
+        public int QuantidadeEmEstoque { get; set; }
+    }
+}
